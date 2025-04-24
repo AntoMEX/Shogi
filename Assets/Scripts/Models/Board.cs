e3513de871f11ff8b412cb3fbea1f774ff5755b1
AntoMEX <antomex.ae@gmail.com>
2025-04-23 18:40:46 -0600

    Initial commit

---
+++ Assets/Scripts/Models/Board.cs
@@ -0,0 +1,44 @@
+
+using Unity.Mathematics;
+using UnityEngine;
+
+
+public struct Board
+{
+    //const int ROWS = 9;
+    //const int COLS = 9;
+
+    Square[,] grid; //Arreglo bidimiensional
+    //Square[][] grid2; Arreglo de arreglos
+    public Board(int rows, int cols) 
+    { 
+        grid = new Square[rows, cols];
+        for(int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                grid[i, j] = new Square(i, j);
+            }
+        }
+        //Debug.Log(grid[3, 0].GetCoor);
+    }
+
+    public ref Square GetSquare(int row, int col) => ref grid[row, col]; //Pide row y column y regresa la copia del square
+
+    //~Board() {}
+}
+
+public struct Square
+{
+    int2 coor; //Unity.Mathematics  enteros bidimensionales
+    //float2 coor; Unity.Mathematics  flotantes bidimensionales
+
+    //public int2 GetCoor => coor;
+    public Square(int x, int y) 
+    { 
+        coor = new int2(x, y);
+    }
+
+    //~Square() {}
+    public int2 Coor => coor;
+}
---
+++ Assets/Scripts/View.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using Unity.Mathematics;
+using TMPro;
+
+public class View : MonoBehaviour
+{
+    [SerializeField] GameObject squarePrefab;
+
+    [SerializeField] Transform gridParent;
+
+    Controller controller;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        controller = new Controller(this);
+    }
+
+    // Update is called once per frame
+    public void CreateGrid(ref Board board, int rows, int cols)
+    {
+        for (int i = 0; i < rows; i++)
+        {
+            for(int j = 0; j < cols; j++)
+            {
+                GameObject newSquare = Instantiate(squarePrefab, gridParent);
+                int2 coor = board.GetSquare(i, j).Coor;
+                newSquare.GetComponentInChildren<TextMeshProUGUI>().text = $"{coor.x},{coor.y}";
+            }
+
+        }
+    }
+
+}
+
+//Solo manda a llamar al controller
